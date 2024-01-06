add35ebfa247b75f6138bb670b504d502fbe40af
JeukHwang <92910273+JeukHwang@users.noreply.github.com>
2024-01-06 15:40:49 +0900

    feat: init react-router-dom

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import Home from "./components/page/Home.tsx";
+import { RouterProvider } from "react-router-dom";
 import "./index.css";
+import router from "./router";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <Home />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 );
